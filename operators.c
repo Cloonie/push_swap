6f69171ead66a7ce849435cd928e96f551990454
Mark Liew <mliew@u90z06s02.42kl.edu.my>
2022-09-22 22:11:28 +0800

    .

--- operators.c
+++ operators.c
@@ -1,32 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   operators.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mliew < mliew@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/16 14:57:45 by mliew             #+#    #+#             */
-/*   Updated: 2022/09/22 21:05:24 by mliew            ###   ########.fr       */
+/*   Created: 2022/09/22 16:38:01 by mliew             #+#    #+#             */
+/*   Updated: 2022/09/22 16:38:23 by mliew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int ac, char **av)
-{
-	int		i;
-	t_stack	*stacka;
-	t_stack	*stackb;
-
-	stacka = ft_split(av[1]);
-	stackb = NULL;
-	i = 1;
-	while (ac > 1 && av[i])
-	{
-		if (atoi(av[i]) == atoi(av[i - 1]))
-			exit (0);
-		ft_printf("%d\n", atoi(av[i]));
-		i++;
-	}
-	// write (1, "\n", 1);
-}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mliew < mliew@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 14:58:03 by mliew             #+#    #+#             */
-/*   Updated: 2022/09/17 18:02:05 by mliew            ###   ########.fr       */
+/*   Updated: 2022/09/22 18:01:11 by mliew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@
 # include "lib42/includes/libft.h"
 # include "lib42/includes/ft_printf.h"
 # include "lib42/includes/get_next_line_bonus.h"
+
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}	t_stack;
 
 #endif
