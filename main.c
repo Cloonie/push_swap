eec0ee982d979f4c5f2041cdd57ae8359df6aadc
Mark Liew <mliew@u82z01s04.42kl.edu.my>
2022-11-29 22:44:06 +0800

    .

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mliew < mliew@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 14:57:45 by mliew             #+#    #+#             */
-/*   Updated: 2022/11/28 21:19:20 by mliew            ###   ########.fr       */
+/*   Updated: 2022/11/29 14:22:22 by mliew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,8 +79,8 @@ void	quick_sort_a(t_list **stacka, t_list **stackb, int size)
 		if (((*stacka)->index <= midval && (size % 2) == 0)
 			|| ((*stacka)->index < midval && (size % 2) == 1))
 			push(stacka, stackb, 'b');
-		else if (check_top_half(stacka, ft_lstsize(*stackb) + 1))
-			rotate(stacka, 'a');
+		// else if (check_top_half(stacka, ft_lstsize(*stackb) + 1))
+		// 	rotate(stacka, 'a');
 		else
 			reverse_rotate(stacka, 'a');
 	}
@@ -141,30 +141,32 @@ int	main(int ac, char **av)
 	stackb = NULL;
 	if (ac == 1)
 		return (0);
+	if (ac == 2)
+		errormsg();
 	stacka = fill_stack(ac, av);
 	check_dup(stacka);
 	size = stacka->size;
 	assign_index(stacka, size);
 	sort_stacks(&stacka, &stackb, size);
 
-	printf("\nStack A:\n");
-	if (stacka == NULL)
-		printf("NULL\n");
-	while (stacka)
-	{
-		printf("Value: %d, Index: %d, Pos: %d\n",
-			stacka->value, stacka->index, stacka->pos);
-		stacka = stacka->next;
-	}
-	printf("\nStack B:\n");
-	if (stackb == NULL)
-		printf("NULL\n");
-	while (stackb)
-	{
-		printf("Value: %d, Index: %d, Pos: %d\n",
-			stackb->value, stackb->index, stackb->pos);
-		stackb = stackb->next;
-	}
+	// printf("\nStack A:\n");
+	// if (stacka == NULL)
+	// 	printf("NULL\n");
+	// while (stacka)
+	// {
+	// 	printf("Value: %d, Index: %d, Pos: %d\n",
+	// 		stacka->value, stacka->index, stacka->pos);
+	// 	stacka = stacka->next;
+	// }
+	// printf("\nStack B:\n");
+	// if (stackb == NULL)
+	// 	printf("NULL\n");
+	// while (stackb)
+	// {
+	// 	printf("Value: %d, Index: %d, Pos: %d\n",
+	// 		stackb->value, stackb->index, stackb->pos);
+	// 	stackb = stackb->next;
+	// }
 
 	// system("leaks push_swap");
 }
